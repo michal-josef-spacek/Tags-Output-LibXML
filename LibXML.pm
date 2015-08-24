6e5ae0b24dd749eae69585146d394d25e09a3a09
Michal Špaček <tupinek@gmail.com>
2015-08-24 12:46:48 +0200

    Add examples.

--- LibXML.pm
+++ LibXML.pm
@@ -315,9 +315,69 @@ __END__
 
  TODO
 
-=head1 EXAMPLE
+=head1 EXAMPLE1
 
- TODO
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use Tags::Output::LibXML;
+
+ # Object.
+ my $tags = Tags::Output::LibXML->new;
+
+ # Put data.
+ $tags->put(
+         ['b', 'text'],
+	 ['d', 'data'],
+	 ['e', 'text'],
+ );
+
+ # Print.
+ print $tags->flush."\n";
+
+ # Output:
+ # <?xml version="1.1" encoding="UTF-8"?>
+ # <text>data</text>
+
+=head1 EXAMPLE2
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use Encode;
+ use Tags::Output::LibXML;
+
+ # Object.
+ my $tags = Tags::Output::LibXML->new(
+         'data_callback' => sub {
+	         my $data_ar = shift;
+		 foreach my $data (@{$data_ar}) {
+		         $data = encode_utf8($data);
+		 }
+                 return;
+	 },
+ );
+
+ # Data in characters.
+ my $data = decode_utf8('řčěšřšč');
+
+ # Put data.
+ $tags->put(
+         ['b', 'text'],
+	 ['d', $data],
+	 ['e', 'text'],
+ );
+
+ # Print.
+ print $tags->flush."\n";
+
+ # Output:
+ # <?xml version="1.1" encoding="UTF-8"?>
+ # <text>řčěšřšč</text>
 
 =head1 DEPENDENCIES
 
