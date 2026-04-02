fe30e10bd61d85fabef6a45a25f77e3a64c45d88
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2026-04-02 08:46:29 +0200

    Prepare for 0.06 version

--- LibXML.pm
+++ LibXML.pm
@@ -11,7 +11,7 @@ use XML::LibXML;
 # Constants.
 Readonly::Scalar my $EMPTY_STR => q{};
 
-our $VERSION = 0.05;
+our $VERSION = 0.06;
 
 # Flush tags in object.
 sub flush {
@@ -475,6 +475,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.05
+0.06
 
 =cut
